465c42056eafb817da80c7611e3805cd94186af5
giaodn <giaodn@dfe69e82-06c8-11de-93dd-cf879171209e>
2009-03-02 11:04:34 +0000

    Modified crawl jobs, added loop function

---
+++ trunk/system/application/views/admin/crawl_jobs_report.php
@@ -0,0 +1,6 @@
+<?=$output?>
+<?if($continue_run && !$is_complete) {?>
+    <script type="text/javascript">
+        setTimeout("window.location.href=window.location.href", 3000);
+    </script>
+<?}?>
