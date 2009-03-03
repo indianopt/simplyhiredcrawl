a3e5965a8a1b951c7f085a92d5ace63991d60572
giaodn <giaodn@dfe69e82-06c8-11de-93dd-cf879171209e>
2009-03-03 11:32:22 +0000

    Added 2 fields: is_crawl_daily_completed and daily_next_url

--- trunk/sql/alter.sql
+++ trunk/sql/alter.sql
@@ -1,1 +1,3 @@
-ALTER TABLE `jobs` ADD `alias` VARCHAR( 128 ) NOT NULL AFTER `name` 
+ALTER TABLE `jobs` ADD `alias` VARCHAR( 128 ) NOT NULL AFTER `name`;
+ALTER TABLE `job_categories` ADD `is_crawl_daily_completed` TINYINT( 1 ) NOT NULL DEFAULT '0' AFTER `is_crawl_completed` ;
+ALTER TABLE `job_categories` ADD `daily_next_url` VARCHAR( 128 ) NOT NULL AFTER `is_crawl_completed` ;
