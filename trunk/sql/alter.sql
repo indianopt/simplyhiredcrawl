35baaa1a4fa64689368cd4a23a5c51fdcd2534e3
giaodn <giaodn@dfe69e82-06c8-11de-93dd-cf879171209e>
2009-03-05 02:16:47 +0000

    Change url of jobs to text

--- trunk/sql/alter.sql
+++ trunk/sql/alter.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `jobs` ADD `alias` VARCHAR( 128 ) NOT NULL AFTER `name`;
 ALTER TABLE `job_categories` ADD `is_crawl_daily_completed` TINYINT( 1 ) NOT NULL DEFAULT '0' AFTER `is_crawl_completed` ;
 ALTER TABLE `job_categories` ADD `daily_next_url` VARCHAR( 128 ) NOT NULL AFTER `is_crawl_completed` ;
+---------------
+ALTER TABLE `jobs` CHANGE `url` `url` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
